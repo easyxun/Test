d2b41009685be5a1d38ed20f8e8af552db73dac2
i7C- <i7C-@i7C-67>
2023-05-02 10:36:43 +0900

    1000 Commit

--- algorithm/src/algorithm/Lv01_1000.java
+++ algorithm/src/algorithm/Lv01_1000.java
@@ -1,10 +1,18 @@
 package algorithm;
 
+import java.util.*;
+
 public class Lv01_1000 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+		Scanner sc = new Scanner(System.in);
+		System.out.println("두 수를 입력하세요:");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = a+b;
+		System.out.println("두 수의 합은 "+c+"입니다.");
+		
 	}
 
 }
